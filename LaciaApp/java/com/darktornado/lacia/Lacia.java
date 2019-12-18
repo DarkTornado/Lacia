549bd20671e64d12ab63009ee6d0166418a2ed5c
Dark Tornado <dt3141592@naver.com>
2019-12-18 11:38:35 +0900

    Lacia 2.1

--- LaciaApp/java/com/darktornado/lacia/Lacia.java
+++ LaciaApp/java/com/darktornado/lacia/Lacia.java
@@ -21,7 +21,7 @@ import java.net.URLConnection;
 
 public class Lacia {
 
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "2.1";
     private static final String sdcard = Environment.getExternalStorageDirectory().getAbsolutePath();
 
     public static int getIcon(){
