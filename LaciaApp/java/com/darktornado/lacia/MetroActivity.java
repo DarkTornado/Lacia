2cf74d48e32eda40556e23f83b6dbe0e0dd01e12
Dark Tornado <dt3141592@naver.com>
2020-06-15 22:07:57 +0900

    Lacia 3.2

--- LaciaApp/java/com/darktornado/lacia/MetroActivity.java
+++ LaciaApp/java/com/darktornado/lacia/MetroActivity.java
@@ -1,8 +1,6 @@
 package com.darktornado.lacia;
 
-import android.content.Intent;
 import android.graphics.Color;
-import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
