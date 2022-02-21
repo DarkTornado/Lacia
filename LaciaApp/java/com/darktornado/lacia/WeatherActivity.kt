11803ef3b3caa30be5b4ceea5a2a47228a714f0f
Dark Tornado <dt3141592@naver.com>
2022-02-21 15:41:27 +0900

    배경색

--- LaciaApp/java/com/darktornado/lacia/WeatherActivity.kt
+++ LaciaApp/java/com/darktornado/lacia/WeatherActivity.kt
@@ -1,10 +1,13 @@
 package com.darktornado.lacia
 
 import android.graphics.Color
-import android.support.v7.app.AppCompatActivity
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.LinearLayout
+import android.widget.Toast
 
 class WeatherActivity : AppCompatActivity() {
 
@@ -14,22 +17,30 @@ class WeatherActivity : AppCompatActivity() {
         val data = intent.getStringExtra("data")
         val layout = LinearLayout(this);
         layout.orientation = 1
-        val toolbar = Lacia.creatTitle(this, pos + " 날씨")
-        setSupportActionBar(toolbar)
-        layout.addView(toolbar)
 
         val web = WebView(this)
-        web.loadData(data, "text/html; charset=UTF-8", null)
-        web.setBackgroundColor(Color.WHITE)
-        val mar = dip2px(20)
-        val margin = LinearLayout.LayoutParams(-1, -1)
-        margin.setMargins(mar, mar, mar, mar)
-        web.layoutParams = margin
+        web.loadUrl("file:///android_asset/weather/index.html")
+        web.settings.javaScriptEnabled = true
+        web.layoutParams = LinearLayout.LayoutParams(-1, -1)
+        web.setBackgroundColor(Color.TRANSPARENT)
+        web.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                web.loadUrl("javascript:applyData(\"$data\")")
+                toast(pos + "의 날씨 정보를 불러왔습니다.")
+                super.onPageFinished(view, url)
+            }
+        }
         layout.addView(web)
-
-        layout.setBackgroundColor(Color.WHITE)
-
+        layout.background = GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, intArrayOf(Color.parseColor("#4FC3F7"), Color.parseColor("#81D4FA"), Color.parseColor("#D1C4E9")))
         setContentView(layout)
+    }
+
+    fun toast(msg: String) {
+        val toast = Toast.makeText(applicationContext, msg, Toast.LENGTH_LONG)
+        toast.view.setBackgroundColor(Lacia.getColor(190))
+        val pad = dip2px(5)
+        toast.view.setPadding(pad, pad, pad, pad)
+        toast.show()
     }
 
     fun dip2px(dips: Int): Int {
